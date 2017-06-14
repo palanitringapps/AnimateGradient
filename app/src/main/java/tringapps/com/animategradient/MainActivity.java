2927e739e9da568b3949949d6607da08d2435b05
palanitringapps <palani.n@tringapps.com>
2017-06-14 10:55:11 +0530

    Initial commit

---
+++ app/src/main/java/tringapps/com/animategradient/MainActivity.java
@@ -0,0 +1,21 @@
+package tringapps.com.animategradient;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.main);
+        AnimationDrawable animationDrawable = (AnimationDrawable)
+                constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(2500);
+        animationDrawable.start();
+    }
+}
